51523031e1a38f37d7e8234b9beda03c68d7d7db
austinmula <maustin64.ma@gmail.com>
2024-08-26 22:47:32 +0300

    footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Urbanist  } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-const urbanist = Urbanist ({ subsets: ["latin"] });
+const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,14 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-slate-900">
-    <body className={clsx(urbanist.className, "relative min-h-screen")}>
-      <Header />
-      {children}
-      <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
-      <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
-      {/* <Footer /> */}
-      {/* <PrismicPreview repositoryName={repositoryName} /> */}
-    </body>
-  </html>
+      <body className={clsx(urbanist.className, "relative min-h-screen")}>
+        <Header />
+        {children}
+        <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
+        <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
+        <Footer />
+        {/* <PrismicPreview repositoryName={repositoryName} /> */}
+      </body>
+    </html>
   );
 }
