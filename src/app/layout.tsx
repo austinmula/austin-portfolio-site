579805f739ccf89376a21420dd531ffb11db002a
austinmula <maustin64.ma@gmail.com>
2024-08-26 22:03:20 +0300

    navbar config and design with prismic page builder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
+import { Urbanist  } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import Header from "@/components/Header";
 
-const urbanist = Urbanist({ subsets: ["latin"] });
+const urbanist = Urbanist ({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-slate-900">
     <body className={clsx(urbanist.className, "relative min-h-screen")}>
-      {/* <Header /> */}
+      <Header />
       {children}
       <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
       <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
